1f5e5d341e25a4426092936c43d8e9e6b9a36eb9
SangWon7242 <gotkdqja@gmail.com>
2025-10-19 11:07:30 +0900

    세팅

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "com.sbs.jsp.board"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("dev.langchain4j:langchain4j:1.7.1")
+    implementation("dev.langchain4j:langchain4j-open-ai:1.7.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
